3465d4a9154e853e6436f52035452422081c3954
MONIQUE5 <anekemonica.am@gmail.com>
2022-09-11 18:06:00 +0100

    new file on loop and variables

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/* include <stdio.h> */
+
+/**
+ * main - entry point
+ *
+ *
+ *
+ * printf: prints string
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+int printf(const char *format, ...);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
