ae86808e22194f8e50e32c18d87c475918d7233c
MONIQUE5 <anekemonica.am@gmail.com>
2022-09-11 16:17:59 +0100

    new file on loop and variables

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/* include <stdio.h> */
+
+/**
+ *main - entry point
+ *
+ *
+ *
+ *
+ * Return: always return 0
+ *
+ */
+
+int main(void)
+{
+int puts(const char *str);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
