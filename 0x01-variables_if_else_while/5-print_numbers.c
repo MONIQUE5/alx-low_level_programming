224fdcad6ca5a8a4010b0ef0fa8e89049102cb80
MONIQUE5 <anekemonica.am@gmail.com>
2022-09-13 00:09:01 +0100

    new file on loop and variables

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *
+ * printf: prints numbers less than 10
+ *
+ *
+ * Return: always returns 0
+ */
+
+int main(void)
+{
+int num;
+for (num = 0; num < 10; num++)
+printf("%d", num);
+printf("\n");
+return (0);
+}
