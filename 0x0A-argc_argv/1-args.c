672fa8af516dd93e0385769a0e6e050414dc7cb9
MONIQUE5 <anekemonica.am@gmail.com>
2022-10-02 02:07:34 +0100

    Projects on Recursion

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main - returns 0
+ * @argc: counts number of arguments in a program
+ * @argv: points to a string in a program
+ *
+ * Return: always returns 0
+ */
+
+int main(int argc, char __attribute__((__unused__)) *argv[])
+{
+printf("%d\n", argc - 1);
+return (0);
+}
